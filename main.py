abc6ae762393a08f6e3d6619493ae4e44208ee3b
Federico Aloi <federico.aloi@gmail.com>
2020-06-01 18:18:27 -0300

    Movido código a una función

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
 import numpy as np
 import cv2
+import textwrap
 
+# Parametros
 plantilla = './plantilla.png'
 titulo = 'Cortacabello Inalambrico Remington Hc5350'
 descripcion = 'Se puede lavar con agua. Cargador USB incluido, pero también se puede usar cualquier cargador de celular. 14 posiciones de corte, desde 3mm hasta 42mm.'
 precio = '4500'
 
-image = cv2.imread(plantilla, cv2.IMREAD_UNCHANGED)
+# Configuración
 font = cv2.FONT_HERSHEY_SIMPLEX
-size = 1
-stroke = 2
+caracteres_por_linea = 50
 
-height, width, _ = image.shape
-
-textsize = cv2.getTextSize(titulo, font, size, stroke)[0]
-
-textX = (width - textsize[0]) // 2
-textY = (height + textsize[1]) // 2
-
-position = (textX, 780)
-cv2.putText(
+def escribir_texto(texto, size, stroke, y):
+  height, width, _ = image.shape
+  textsize = cv2.getTextSize(titulo, font, size, stroke)[0]
+  x = (width - textsize[0]) // 2
+  position = (x, y)
+  cv2.putText(
      image, 
-     titulo,
+     texto,
      position,
      font, 
      size, 
      (255, 255, 255, 255),
      stroke) 
+
+image = cv2.imread(plantilla, cv2.IMREAD_UNCHANGED)
+
+escribir_texto(titulo, 1, 2, 780)
+
 cv2.imshow('image',image)
 cv2.waitKey(0)
