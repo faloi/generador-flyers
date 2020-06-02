7fa2e2ad7963bce7d51e0924b330d32520b82936
Federico Aloi <federico.aloi@gmail.com>
2020-06-02 18:38:56 -0300

    Ajustado tamaño y removidos acentos

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import numpy as np
 import cv2
 import textwrap
+from unidecode import unidecode
 
 # Parametros
 plantilla = './plantilla.png'
@@ -14,31 +15,32 @@ precio = '4500'
 font = cv2.FONT_HERSHEY_DUPLEX
 caracteres_por_linea = 50
 interlineado = 10
-siguiente_posicion = 775
+siguiente_posicion = 800
+font_base_size = 1.1
 
 def escribir_texto(texto, size, stroke, espacio_adicional = 0):
   global siguiente_posicion
+  final_size = font_base_size * size
   _, width, _ = image.shape
-  sizeX, sizeY = cv2.getTextSize(texto, font, size, stroke)[0]
+  sizeX, sizeY = cv2.getTextSize(texto, font, final_size, stroke)[0]
   x = (width - sizeX) // 2
-  position = (x, siguiente_posicion)
+  position = (x, siguiente_posicion + espacio_adicional)
   cv2.putText(
      image, 
-     texto,
+     unidecode(texto),
      position,
      font, 
-     size, 
+     final_size, 
      (255, 255, 255, 255),
      stroke)
   siguiente_posicion += sizeY + interlineado + espacio_adicional
 
 image = cv2.imread(plantilla, cv2.IMREAD_UNCHANGED)
 
-escribir_texto(titulo, 1, 2)
-escribir_texto(f"${precio}", 1, 2)
+escribir_texto(titulo, 1.2, 2)
+escribir_texto(f"${precio}", 2, 2, 2 * interlineado)
 
 for linea in textwrap.wrap(descripcion, caracteres_por_linea):
-  escribir_texto(linea, 0.8, 1)
+  escribir_texto(linea, 1, 1)
 
-cv2.imshow('image', image)
-cv2.waitKey(0)
+cv2.imwrite('./out.png', image)
