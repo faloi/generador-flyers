f44937ce55337dee1c5ac417df2abbebf4181adf
Federico Aloi <federico.aloi@gmail.com>
2020-06-03 21:27:05 -0300

    Movida funcion generar a Editor

--- main.py
+++ main.py
@@ -18,8 +18,26 @@ posicion_inicial_y = 800
 
 
 class Editor:
-  def __init__(self, posicion_inicial):
+  def __init__(self, posicion_inicial, titulo, precio, descripcion):
     self.siguiente_posicion = posicion_inicial
+    self.titulo = titulo
+    self.precio = precio
+    self.descripcion = descripcion
+
+  def generar_flyer(self):
+    etiqueta = self.generar_etiqueta()
+    cv2.imwrite(f"out/{self.titulo}.png", etiqueta)
+
+  def generar_etiqueta(self):
+    etiqueta = cv2.imread(plantilla, cv2.IMREAD_UNCHANGED)
+
+    self.escribir_texto(etiqueta, self.titulo, 1.2, 2)
+    self.escribir_texto(etiqueta, f"${self.precio}", 2, 2, 2 * interlineado)
+
+    for linea in textwrap.wrap(self.descripcion, caracteres_por_linea):
+      self.escribir_texto(etiqueta, linea, 1, 1)
+
+    return etiqueta
 
   def escribir_texto(self, image, texto, size, stroke, espacio_adicional=0):
     final_size = font_base_size * size
@@ -38,22 +56,10 @@ class Editor:
     self.siguiente_posicion += sizeY + interlineado + espacio_adicional
 
 
-def generar_imagen(titulo, precio, descripcion):
-  image = cv2.imread(plantilla, cv2.IMREAD_UNCHANGED)
-  editor = Editor(posicion_inicial_y)
-
-  editor.escribir_texto(image, titulo, 1.2, 2)
-  editor.escribir_texto(image, f"${precio}", 2, 2, 2 * interlineado)
-
-  for linea in textwrap.wrap(descripcion, caracteres_por_linea):
-    editor.escribir_texto(image, linea, 1, 1)
-
-  cv2.imwrite(f"out/{titulo}.png", image)
-
-
 with open('inventario.csv', mode='r') as csv_file:
   csv_reader = csv.DictReader(csv_file)
   for row in csv_reader:
     if row['¿Vender ahora?'] == 'Sí':
-      generar_imagen(
-          titulo=row['Nombre'], descripcion=row['Descripción'], precio=row['Valor estimado'])
+      editor = Editor(titulo=row['Nombre'], descripcion=row['Descripción'],
+                      precio=row['Valor estimado'], posicion_inicial=posicion_inicial_y)
+      editor.generar_flyer()
