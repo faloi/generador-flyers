09d9593c2dd627fac5e9e3ee22802a5ded270454
Federico Aloi <federico.aloi@gmail.com>
2020-06-03 22:12:33 -0300

    Se arma el flyer combinando las imágenes

--- main.py
+++ main.py
@@ -14,19 +14,39 @@ font = cv2.FONT_HERSHEY_DUPLEX
 caracteres_por_linea = 50
 interlineado = 10
 font_base_size = 1.1
-posicion_inicial_y = 800
 
 
 class Editor:
-  def __init__(self, posicion_inicial, titulo, precio, descripcion):
-    self.siguiente_posicion = posicion_inicial
+  def __init__(self, titulo, precio, descripcion, ruta_foto):
+    self.siguiente_posicion = 800
     self.titulo = titulo
     self.precio = precio
     self.descripcion = descripcion
+    self.ruta_foto = ruta_foto
 
   def generar_flyer(self):
     etiqueta = self.generar_etiqueta()
-    cv2.imwrite(f"out/{self.titulo}.png", etiqueta)
+    foto = self.leer_foto()
+    flyer = self.combinar(foto, etiqueta)
+    cv2.imwrite(f"out/{self.titulo}.png", flyer)
+
+  def leer_foto(self):
+    foto = cv2.imread(self.ruta_foto, cv2.IMREAD_UNCHANGED)
+    return cv2.copyMakeBorder(foto, 0, 1080 - foto.shape[0], 0, 0, cv2.BORDER_CONSTANT)
+
+  def combinar(self, src, overlay, pos=(0, 0)):
+    h, w, _ = overlay.shape
+    rows, cols, _ = src.shape
+    x, y = pos[0], pos[1]
+ 
+    for i in range(h):
+      for j in range(w):
+        if x + i >= rows or y + j >= cols:
+            continue
+        alpha = float(overlay[i][j][3] / 255.0)
+        src[x + i][y + j] = alpha * overlay[i][j][:3] + \
+            (1 - alpha) * src[x + i][y + j]
+    return src
 
   def generar_etiqueta(self):
     etiqueta = cv2.imread(plantilla, cv2.IMREAD_UNCHANGED)
@@ -61,5 +81,5 @@ with open('inventario.csv', mode='r') as csv_file:
   for row in csv_reader:
     if row['¿Vender ahora?'] == 'Sí':
       editor = Editor(titulo=row['Nombre'], descripcion=row['Descripción'],
-                      precio=row['Valor estimado'], posicion_inicial=posicion_inicial_y)
+                      precio=row['Valor estimado'], ruta_foto='in/out.jpg')
       editor.generar_flyer()
